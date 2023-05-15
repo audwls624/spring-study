30649d3bd03dc0973f1997fb1289f7f3e582a354
nikola <nikola@vlending.co.kr>
2023-05-15 15:06:07 +0900

    service repository 자동 의존관계 설정

--- hello-spring/src/main/java/hello/hellospring/SpringConfig.java
+++ hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -10,15 +10,15 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class SpringConfig {
 
-    @Bean
-    public MemberService memberService() {
-        return new MemberService(memberRepository());
-    }
-
-    @Bean
-    public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
-//        return new DBMemberRepository(); // 구현 클래스를 변경해야되는 경우 이런식으로 등록 가능
-    }
+//    @Bean
+//    public MemberService memberService() {
+//        return new MemberService(memberRepository());
+//    }
+//
+//    @Bean
+//    public MemberRepository memberRepository() {
+//        return new MemoryMemberRepository();
+////        return new DBMemberRepository(); // 구현 클래스를 변경해야되는 경우 이런식으로 등록 가능
+//    }
 
 }
--- hello-spring/src/main/java/hello/hellospring/service/MemberService.java
+++ hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -9,7 +9,8 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
 
-//@Service
+
+@Service
 public class MemberService {
     private final MemberRepository memberRepository;
 
