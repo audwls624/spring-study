756289647c68e2e4ff7c58e2d7e97dd59eb642bd
nikola <nikola@vlending.co.kr>
2023-05-15 15:02:18 +0900

    스프링 빈 등록 수동 등록(SpringConfig) 추가

---
+++ hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -0,0 +1,24 @@
+package hello.hellospring;
+
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+import hello.hellospring.service.MemberService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class SpringConfig {
+
+    @Bean
+    public MemberService memberService() {
+        return new MemberService(memberRepository());
+    }
+
+    @Bean
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+//        return new DBMemberRepository(); // 구현 클래스를 변경해야되는 경우 이런식으로 등록 가능
+    }
+
+}
--- hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
@@ -9,6 +9,8 @@ public class MemberController {
     private final MemberService memberService;
 
     @Autowired
+    // 의존 관계가 실행중에 동적으로 변하는 경우는 거의 없으므로 생성자 주입 권장(필드 주입, setter 주입, 생성자 주입)
+    // `Autowired`를 통한 DI는 스프링이 관리하는 객체에서만 동작한다.
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
     }
