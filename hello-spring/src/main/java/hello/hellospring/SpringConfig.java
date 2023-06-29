4a7f57257d49b97024477e889366e10f50061a79
jin <audwls624@gmail.com>
2023-06-30 00:07:13 +0900

    스프링 데이터 JPA 적용

--- hello-spring/src/main/java/hello/hellospring/SpringConfig.java
+++ hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -15,26 +15,35 @@ import javax.sql.DataSource;
 @Configuration
 public class SpringConfig {
 
-    private EntityManager em;
+    private final MemberRepository memberRepository;
 
     @Autowired
-    public SpringConfig(EntityManager em) {
-        this.em = em;
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
 
+    //    private EntityManager em;
+//
+//    @Autowired
+//    public SpringConfig(EntityManager em) {
+//        this.em = em;
+//    }
+
     @Bean
-    public MemberService memberService() { return new MemberService(memberRepository()); }
+    public MemberService memberService() {
+        return new MemberService(memberRepository);
+    }
 
 //    @Bean
 //    public MemberService memberService() {
 //        return new MemberService(memberRepository());
 //    }
 //
-    @Bean
-    public MemberRepository memberRepository() {
-        return new JpaMemberRepository(em);
+//    @Bean
+//    public MemberRepository memberRepository() {
+//        return new JpaMemberRepository(em);
 //        return new MemoryMemberRepository();
 //        return new DBMemberRepository(); // 구현 클래스를 변경해야되는 경우 이런식으로 등록 가능
-    }
+//    }
 
 }
